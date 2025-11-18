87876cf1d7b102127328b3c51f5d1ea4f98e42a2
Yunseok/Isaac Jang <yunseokbb@gmail.com>
2025-11-18 09:20:47 +0900

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "경비 계산기 (Trip Expenses Calculator)",
+  title: "경비 계산기",
 };
 
 export default function RootLayout({
